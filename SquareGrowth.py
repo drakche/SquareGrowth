d6af6cbfccc48379b9548e76ccf26a8c73d0917f
Dragan Stojanović <drakche.69@gmail.com>
2014-09-13 19:41:57 +0200

    development set up

--- SquareGrowth.py
+++ SquareGrowth.py
@@ -1,8 +1,16 @@
 from kivy.app import App
+from kivy.uix.button import Button
+
 
 class TutorialApp(App):
-    pass
-
+    def build(self):
+        return Button(
+            text='Hello!',
+            backgraound_color=(0, 0, 1, 1),
+            font_size=15
+            )
 
 if __name__ == "__main__":
     TutorialApp().run()
+
+
