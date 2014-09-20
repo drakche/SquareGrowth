a629b6f62ec31fccab183bf05acf1cf61c2e333a
Dragan Stojanović <drakche.69@gmail.com>
2014-09-20 20:58:45 +0200

    positioning breakthoruhg
    made the random positioning part, now the growth and clicking

--- SquareGrowth.py
+++ SquareGrowth.py
@@ -2,55 +2,40 @@
 # -*- coding: utf-8 -*-
 
 from kivy.app import App
-
-from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.widget import Widget
 from kivy.uix.button import Button
+from kivy.uix.scatter import Scatter
+from kivy.uix.label import Label
+from kivy.uix.floatlayout import FloatLayout
+from kivy.core.window import Window
 from kivy.properties import ListProperty, ObjectProperty
-from kivy.graphics.vertex_instructions import Rectangle, Ellipse, Line
-from kivy.graphics.context_instructions import Color
-from kivy.clock import Clock
-from functools import partial
 import random
 
 
-class ScatterTextWidget(BoxLayout):
+class Square(Widget):
 
-    text_color = ObjectProperty([1, 0, 0, 1])
-    scatter_size_x = ObjectProperty(1)
-    scatter_size_y = ObjectProperty(1)
-    scatter_pos = ObjectProperty([150, 150])
-    growth_rate = 0.25
-    pressed = False
-
-    def __init__(self, **kwargs):
-        super(ScatterTextWidget, self).__init__(**kwargs)
-
-        def grow(*args):
-            self.scatter_size_x += self.growth_rate
-            self.scatter_size_y += self.growth_rate
-
-        Clock.schedule_interval(grow, 0.017)
-
-    def toggle_growth(self, *args):
-        if self.pressed:
-            if self.growth_rate == 0:
-                self.growth_rate = 0.25
-            else:
-                self.growth_rate = 0
-            self.pressed = False
-        else:
-            self.pressed = True
-
-    def change_label_color(self, *args):
-        c = [random.random() for i in range(4)]
-        self.text_color = c
+    pass
 
 
-class TutorialApp(App):
+class Squatter(Widget):
+
+    pass
+
+
+class MainScreenScatter(FloatLayout):
+
+    def getRandPos(*args):
+        return [random.random() * Window.width, random.random()
+                * Window.height]
+
+    pass
+
+
+class GrowthApp(App):
 
     def build(self):
-        return ScatterTextWidget()
+        return MainScreenScatter()
 
 
 if __name__ == '__main__':
-    TutorialApp().run()
+    GrowthApp().run()
