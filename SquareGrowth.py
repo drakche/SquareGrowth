8f3feca1e2aac195f8c9e1bb3a091d3a758b62a5
Dragan Stojanović <drakche.69@gmail.com>
2014-09-21 14:10:14 +0200

    growth and clicking done
    colision needed

--- SquareGrowth.py
+++ SquareGrowth.py
@@ -9,17 +9,48 @@ from kivy.uix.label import Label
 from kivy.uix.floatlayout import FloatLayout
 from kivy.core.window import Window
 from kivy.properties import ListProperty, ObjectProperty
+from kivy.clock import Clock
+from kivy.animation import Animation
 import random
 
 
 class Square(Widget):
 
-    pass
+    growth = 0.1
+    velocity = ListProperty([10, 15])
+    moved = False
+
+    def __init__(self, **kwargs):
+        super(Square, self).__init__(**kwargs)
+        Clock.schedule_interval(self.update, 1 / 60.)
+
+    def update(self, *args):
+        self.width += self.growth
+
+        self.height += self.growth
 
 
-class Squatter(Widget):
+        # if (self.x + self.width) > Window.width:
+        #     self.x -= self.growth
+        # if (self.y + self.height) > Window.height:
+        #     self.y -= self.growth
+        # if self.x - self.width <0:
+        #     self.x += self.growth
+        # if self.y - self.height <0:
+        #     self.y += self.growth
 
-    pass
+
+    def on_touch_move(self, touch):
+        self.moved = True
+
+    def on_touch_up(self, touch):
+        if self.moved == False:
+            if self.growth != 0:
+                self.growth = 0
+            else:
+                self.growth = 0.1
+        else:
+            self.moved = False
 
 
 class MainScreenScatter(FloatLayout):
